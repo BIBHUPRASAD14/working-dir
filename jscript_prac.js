5771f0a1b2fb8c87c0045bf3a6b07f86456d226f
BIBHUPRASAD14 <bibhuprasadb868@gmail.com>
2023-02-15 23:45:07 +0530

    if else conditionals

--- jscript_prac.js
+++ jscript_prac.js
@@ -1,37 +1,16 @@
-// all primitive datatypes
-// nn bb ss u
-
-let a = null                            // null datatype
-let b = 234                             // number datatype
-let c = true                            // boolean datatype
-let d = BigInt(523)                     // bigint datatype
-let x = "Bibhu"                         // string datatype
-let y = Symbol("this is a symbol")      // symbol datatype
-let z                                   // undefined datatype
-
-console.log( a, b, c, d, x, y, z)
+// age detection for driving a car
+// using if, else and else if
 
 
+let age = prompt("What is your age?: ")
+age = Number.parseInt(age)
 
-// concatinating a string with a num
-
-a = "Bibhuprasad"
-b = 55
-
-console.log( a + b ) 
-
-
-
-// creating a object of dictionary having 5 words
-
-let dict = {
-    Hiraeth: "Homesickness for a place that cannot be returned to.",
-    Serendipity: "Finding something good without looking for it.",
-    Onomatopoeia: "A word that sounds like its meaning.",
-    Petrichor: "The pleasant smell of rain after a long period of dry weather.",
-    Euphoria: "A feeling of intense happiness and excitement."
-
+if (age <= 1){
+    alert()
 }
-
-console.log(dict.Serendipity)       // printing the meaning of Serendipity
-console.log(dict['Serendipity'])     // also can be accessed with this syntex     
+else if (age > 1 && age < 18 ){
+    alert("You should not drive a car, you are underage!!")
+}
+else{
+    alert("You should drive a car!!")
+}
