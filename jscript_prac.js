8766bf2e758041d2c1ee384037f208c981b2b6bf
BIBHUPRASAD14 <bibhuprasadb868@gmail.com>
2023-02-15 20:43:05 +0530

    datatypes in js

---
+++ jscript_prac.js
@@ -0,0 +1,12 @@
+// all data types
+// nn bb ss u
+
+let a = null                        // null dattatype
+let b = 234                         // number datatype
+let c = true                        // boolean datatype
+let d = BigInt(527)                 // bigint datatype
+let x = "Bibhu"                     // string datatype
+let y = Symbol("this is a symbol")  // symbol datatype
+let z                               //undefined datatype
+
+console.log( a, b, c, d, x, y, z)
