6db39c681561ace6ef1f9f317d764c1e5841d81a
BIBHUPRASAD14 <bibhuprasadb868@gmail.com>
2023-02-16 20:22:19 +0530

    update

--- jscript_prac.js
+++ jscript_prac.js
@@ -1,16 +1,16 @@
 // age detection for driving a car
 // using if, else and else if
 
-
+const prompt = require("prompt-sync")({ sigint : true })
 let age = prompt("What is your age?: ")
 age = Number.parseInt(age)
 
-if (age <= 1){
-    alert()
+if (age <= 0){
+    console.log("Invalid age!!-")
 }
 else if (age > 1 && age < 18 ){
-    alert("You should not drive a car, you are underage!!")
+    console.log("You should not drive a car, you are underage!!")
 }
 else{
-    alert("You should drive a car!!")
+    console.log("You should drive a car!!")
 }
