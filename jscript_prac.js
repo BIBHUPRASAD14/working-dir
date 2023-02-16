f75bb8004b59b6e4dadef60d7c2cefd2731b27c2
BIBHUPRASAD14 <bibhuprasadb868@gmail.com>
2023-02-16 19:56:19 +0530

    conditionals updated

--- jscript_prac.js
+++ jscript_prac.js
@@ -1,12 +1,16 @@
-// all data types
-// nn bb ss u
+// age detection for driving a car
+// using if, else and else if
 
-let a = null                        // null dattatype
-let b = 234                         // number datatype
-let c = true                        // boolean datatype
-let d = BigInt(527)                 // bigint datatype
-let x = "Bibhu"                     // string datatype
-let y = Symbol("this is a symbol")  // symbol datatype
-let z                               //undefined datatype
+const prompt = require("prompt-sync")({ sigint : true })
+let age = prompt("What is your age?: ")
+age = Number.parseInt(age)
 
-console.log( a, b, c, d, x, y, z)
+if (age <= 0){
+    console.log("Invalid age!!-")
+}
+else if (age > 1 && age < 18 ){
+    console.log("You should not drive a car, you are underage!!")
+}
+else{
+    console.log("You should drive a car!!")
+}
