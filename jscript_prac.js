6f949752e59daed9dba95a97eb50f5262fee1f63
BIBHUPRASAD14 <bibhuprasadb868@gmail.com>
2023-02-20 23:04:02 +0530

    creat function

--- jscript_prac.js
+++ jscript_prac.js
@@ -1,16 +1,41 @@
-const prompt = require("prompt-sync")({ sigint : true })
-age = Number.parseInt(prompt("what's your age?: "))     
-if (age < 18){
-    console.log("you cannot drive")
+// creating a function
+// functions has parameters
+
+function avg(a,b){
+    return (a + b) / 2          // Math.round() can be used to round up the value
+    console.log("Done!!")
 }
 
-// in special cases switch can be used
-else{
-    switch (age){
-        case 18:
-            console.log("your age is undecidable!!")
-            break
-        default:
-            console.log("You can drive!!")
-    }   
+// define variables and use the function created
+
+let x = 5
+let y = 2
+console.log("avg of x and y is ", avg(x,y))
+
+// function helps to write once execute multiple num of times
+
+                                                                // check prompt-sync module is installed
+                                                                // if not use npm install prompt-sync
+const prompt = require("prompt-sync")({ sigint : true})         // remember this line of code to use prompt in vs code
+
+
+let first_num = Number.parseInt(prompt("Enter the first num: "))
+let sec_num = Number.parseInt(prompt("Enter the second num: "))
+console.log("avg of the numbers entered is ", avg(first_num,sec_num))
+
+// we can use function like this
+
+// another advanced method of creating a function
+
+const sum = (p , q) =>{
+    return p + q
 }
+
+console.log(sum(7,4)) 
+
+// eg :- 
+
+const hello = () =>{
+    console.log("hello!!")
+}
+console.log(hello())
