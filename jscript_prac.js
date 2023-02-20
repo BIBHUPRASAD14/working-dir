78cf431c55229468acbab38b7f928d81f7d6c265
BIBHUPRASAD14 <bibhuprasadb868@gmail.com>
2023-02-21 03:26:35 +0530

    create functions

--- jscript_prac.js
+++ jscript_prac.js
@@ -1,41 +1,14 @@
-// creating a function
-// functions has parameters
+// backtiks are used to insert variables along with string
 
-function avg(a,b){
-    return (a + b) / 2          // Math.round() can be used to round up the value
-    console.log("Done!!")
-}
+let name = "bibhuprasad"
 
-// define variables and use the function created
+console.log(`My name is ${name}`)
 
-let x = 5
-let y = 2
-console.log("avg of x and y is ", avg(x,y))
+// backslash used in js to show special characters
 
-// function helps to write once execute multiple num of times
+//  \n = new line
+//  \t = new tab
+//  \r = carraige return 
 
-                                                                // check prompt-sync module is installed
-                                                                // if not use npm install prompt-sync
-const prompt = require("prompt-sync")({ sigint : true})         // remember this line of code to use prompt in vs code
+console.log(" hello. \n new line. \t tab. \r carraige return")
 
-
-let first_num = Number.parseInt(prompt("Enter the first num: "))
-let sec_num = Number.parseInt(prompt("Enter the second num: "))
-console.log("avg of the numbers entered is ", avg(first_num,sec_num))
-
-// we can use function like this
-
-// another advanced method of creating a function
-
-const sum = (p , q) =>{
-    return p + q
-}
-
-console.log(sum(7,4)) 
-
-// eg :- 
-
-const hello = () =>{
-    console.log("hello!!")
-}
-console.log(hello())
