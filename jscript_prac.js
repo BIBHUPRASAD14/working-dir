ee6cba4b7ada0c10e7fc7f4b69d3148e032f4d5e
BIBHUPRASAD14 <bibhuprasadb868@gmail.com>
2023-03-03 16:08:49 +0530

    js promises

--- jscript_prac.js
+++ jscript_prac.js
@@ -13,4 +13,4 @@ p.then((message) => {                           // then func is to execute
     console.log(message,", it is done.")        // print yes if done
 }).catch((message) => {                         // catch func is to catch errors
     console.log(message,", it is not done.")    // print no if not done
-})
+}) 
